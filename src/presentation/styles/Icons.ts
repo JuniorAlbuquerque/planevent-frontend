c246e288433337d6e6d9411c450f8405924e50d6
JuniorAlbuquerque <junioralbuquerque81@gmail.com>
2020-12-24 20:18:22 -0300

    chore: add assets and icons

---
+++ src/presentation/styles/Icons.ts
@@ -0,0 +1,6 @@
+export { AiOutlineDelete } from 'react-icons/ai';
+export { BiEdit } from 'react-icons/bi';
+export { FiLogIn, FiSearch, FiFilter, FiInfo } from 'react-icons/fi';
+export { MdDateRange, MdAdd } from 'react-icons/md';
+export { GrLocation, GrDiamond } from 'react-icons/gr';
+export { FaSlideshare } from 'react-icons/fa';
