7f5708f9261ef1f89f22a1d22bb583a6615c499f
JuniorAlbuquerque <junioralbuquerque81@gmail.com>
2020-12-29 01:15:00 -0300

    chore: create actually events data

--- src/presentation/renders/ActuallyEvents/index.tsx
+++ src/presentation/renders/ActuallyEvents/index.tsx
@@ -1,17 +1,48 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Card, CardHeader, CardBody, DateEvent, Local } from './styles';
 
-import EmptyList from '../EmptyList';
+import { FaSlideshare, GrDiamond } from '../../styles/Icons';
 
 const ActuallyEvents: React.FC = () => {
-  var mostrar = 'sim';
-
   return (
     <Container>
-      {
-        mostrar === 'não' ? <EmptyList /> : <h1>Olá</h1>
-      }
+      <Card>
+        <CardHeader>
+          <h2>PG Elias</h2>
+
+          <FaSlideshare size={30} color={'var(--blueLight)'} />
+        </CardHeader>
+
+        <CardBody>
+          <p>
+            Pequeno grupo criado pelos membros da Igreja do Nogueira Júnior com o objetivo de estudar e aprender mais sobre a palavra de Deus
+          </p>
+        </CardBody>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <h2>Treinamento Desbravadores</h2>
+
+          <GrDiamond className="exclusive" size={30} color={'var(--blueLight)'} />
+        </CardHeader>
+
+        <CardBody>
+          <p>
+          Orientações para o ministério dos desbravadores, tratamento de questões administrativas necessárias para o bom andamento da Igreja.
+          Repassaremos o protótipo de calendário antecipado por nossa Associação e a ênfase que daremos em 2021.
+          </p>
+
+          <DateEvent>
+
+          </DateEvent>
+
+          <Local>
+
+          </Local>
+        </CardBody>
+      </Card>
     </Container>
   );
 }
--- src/presentation/renders/ActuallyEvents/styles.ts
+++ src/presentation/renders/ActuallyEvents/styles.ts
@@ -1,5 +1,91 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: grid;
+  margin: 0px 14px 20px;
+  grid-gap: 30px;
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  justify-items: baseline;
+  justify-content: baseline;
+  align-items: baseline;
+  transition: 0.5s ease;
+
+  @media(max-width: 986px) {
+    justify-items: center;
+    justify-content: center;
+  }
+`;
+
+export const Card = styled.div`
+  background: var(--primary);
+  width: 340px;
+  height: 400px;
+  max-height: 100%;
+  overflow: auto;
+
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+
+  color: var(--white);
+  padding: 14px;
+
+  /* width */
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: var(--purpleLight);
+  }
+
+  /* Handle on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: #5C54D9;
+    transition: .3s ease-out;
+  }
+`;
+
+export const CardHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-bottom: 8px;
+
+  h2 {
+    font-weight: 600;
+  }
+
+  .exclusive {
+    width: 40px;
+
+    path {
+      stroke: var(--blueLight);
+    }
+  }
+`;
+
+export const CardBody = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  p {
+    text-align: justify;
+  }
+`;
+
+export const DateEvent = styled.div`
 
 `;
+
+export const Local = styled.div`
+
+`;
+
