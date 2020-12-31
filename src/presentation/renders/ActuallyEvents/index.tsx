c3cd7bed2088733d259c450635657b823816c19d
JuniorAlbuquerque <junioralbuquerque81@gmail.com>
2020-12-31 00:43:38 -0300

    chore: create get events

--- src/presentation/renders/ActuallyEvents/index.tsx
+++ src/presentation/renders/ActuallyEvents/index.tsx
@@ -1,27 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../../infra/services/api';
+import { useAuth } from '../../../data/hooks/AuthContext';
+import Moment from 'moment';
+
+import EmptyList from '../EmptyList';
 
 import { Container } from './styles';
 import Card from '../../components/Card';
 
-const ActuallyEvents: React.FC = () => {
-  return (
-    <Container>
-      <Card
-        name='PG Elias'
-        description='Pequeno grupo criado pelos membros da Igreja do Nogueira Júnior com o objetivo de estudar e aprender mais sobre a palavra de Deus'
-        date='14/12/2020'
-        local= 'Rua Eduardo Ribeiro, nº 3895, Jauary 2'
-        type='Compartilhado'
-      />
+interface Events {
+  id: number;
+  name: string;
+  description: string;
+  local: string;
+  date: string;
+  type: string;
+}
 
-      <Card
-        name='PG Elias'
-        description='Pequeno grupo criado pelos membros da Igreja do Nogueira Júnior com o objetivo de estudar e aprender mais sobre a palavra de Deus'
-        date='14/12/2020'
-        local= 'Rua Eduardo Ribeiro, nº 3895, Jauary 2'
-        type='Compartilhado'
-      />
-    </Container>
+const ActuallyEvents: React.FC = () => {
+  const { user } = useAuth();
+  const [eventsUser, setEventsUser] = useState<Events[]>([]);
+
+  useEffect(() => {
+    api.get<Events[]>(`/userevent/actually/${user.id}`)
+    .then(response => {
+        setEventsUser(response.data);
+    })
+  }, [user.id]);
+
+  return (
+    <>
+     {eventsUser.length > 0 ?
+      <Container>
+        {
+          eventsUser.map(ev => (
+            <Card
+              key={ev.id}
+              name={ev.name}
+              description={ev.description}
+              date={Moment(ev.date).format('DD/MM/YYYY')}
+              local={ev.local}
+              type={ev.type}
+            />
+          ))
+        }
+      </Container> :
+      <EmptyList />
+    }
+    </>
   );
 }
 
--- src/presentation/renders/NextEvents/index.tsx
+++ src/presentation/renders/NextEvents/index.tsx
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../../infra/services/api';
+import { useAuth } from '../../../data/hooks/AuthContext';
+import Moment from 'moment';
 
 import { Container } from './styles';
 
 import Card from '../../components/Card';
 import EmptyList from '../EmptyList';
 
-const NextEvents: React.FC = () => {
-  return (
-    <EmptyList />
+interface Events {
+  id: number;
+  name: string;
+  description: string;
+  local: string;
+  date: string;
+  type: string;
+}
 
-    // <Container>
-    //     <Card
-    //     name='PG Elias'
-    //     description='Pequeno grupo criado pelos membros da Igreja do Nogueira Júnior com o objetivo de estudar e aprender mais sobre a palavra de Deus'
-    //     date='14/12/2020'
-    //     local= 'Rua Eduardo Ribeiro, nº 3895, Jauary 2'
-    //     type='Compartilhado'
-    //   />
-    // </Container>
+
+const NextEvents: React.FC = () => {
+  const { user } = useAuth();
+  const [eventsUser, setEventsUser] = useState<Events[]>([]);
+
+  useEffect(() => {
+    api.get<Events[]>(`/userevent/next/${user.id}`)
+    .then(response => {
+        setEventsUser(response.data);
+    })
+  }, [user.id]);
+
+  return (
+    <>
+    {eventsUser.length > 0 ?
+     <Container>
+       {
+         eventsUser.map(ev => (
+           <Card
+             key={ev.id}
+             name={ev.name}
+             description={ev.description}
+             date={Moment(ev.date).format('DD/MM/YYYY')}
+             local={ev.local}
+             type={ev.type}
+           />
+         ))
+       }
+     </Container> :
+     <EmptyList />
+   }
+   </>
   );
 }
 
