18c6dd1ae1b51e54c3d082d5b20dbffc20277a91
JuniorAlbuquerque <junioralbuquerque81@gmail.com>
2021-01-03 01:01:10 -0300

    chore: create page edit profile

---
+++ src/presentation/pages/EditProfile/index.tsx
@@ -0,0 +1,128 @@
+import React, { useCallback, useState, useEffect } from 'react';
+import { Form } from '@unform/web';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { useUpAndDel } from '../../../data/hooks/account';
+
+import { Container, Wrapper, Header, Content, Row, ContentRow, Buttons } from './styles';
+import { UserData } from '../../../data/protocols/user';
+
+import api from '../../../infra/services/api';
+import Sidebar from '../../components/Sidebar';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import Button from '../../components/Button';
+
+import { AiOutlineDelete } from '../../styles/Icons';
+
+interface UserId {
+  id_user: string;
+}
+
+const EditProfile: React.FC = () => {
+  const { updateUser, deleteUser } = useUpAndDel();
+  const { params } = useRouteMatch<UserId>();
+  const [userdata, setUserData] = useState<UserData>({} as UserData);
+  const sextype = ['Masculino', 'Feminino'];
+
+  const handleSubmit = useCallback(async (data: UserData) => {
+    updateUser({
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      birthdate: data.birthdate,
+      sex: data.sex
+    });
+  }, [updateUser]);
+
+  useEffect(() => {
+    api.get(`/user/${params.id_user}`)
+      .then(response => {
+        setUserData(response.data);
+      });
+  }, [params.id_user]);
+
+  return (
+    <Container>
+      <Sidebar />
+      <Wrapper>
+        <Header>
+          <div>
+            <Link to='/home'>
+            {'< Home '}
+            </Link>
+
+            <h3>Gerenciar conta</h3>
+          </div>
+
+          <Button onClick={() => deleteUser(params.id_user)}>
+            Excluir conta
+            <AiOutlineDelete size={20}/>
+          </Button>
+        </Header>
+
+        <Content>
+          <Form onSubmit={handleSubmit}>
+            <Row>
+              <ContentRow>
+                <label htmlFor="">Nome</label>
+                <Input name="name" value={userdata.name} required/>
+              </ContentRow>
+            </Row>
+
+            <Row>
+              <ContentRow>
+                <label htmlFor="">Email</label>
+                <Input name="email" value={userdata.email} required/>
+              </ContentRow>
+            </Row>
+
+            <Row>
+              <ContentRow>
+                <label htmlFor="">Nova Senha</label>
+                <Input name="password" type="password" required/>
+              </ContentRow>
+
+              <Input type="hidden" name="id" value={userdata.id} />
+            </Row>
+
+            <Row>
+              <ContentRow>
+                <label htmlFor="type">Sexo</label>
+                <Select name="sex" required>
+                  {
+                    sextype.map(item =>
+                      item === userdata.sex ?
+                      <option
+                        defaultValue={item}
+                        value={item}>{item}
+                      </option> :
+                      <option value={item}>{item}</option>
+                    )
+                  }
+                </Select>
+              </ContentRow>
+
+              <ContentRow>
+                <label htmlFor="">Data de nascimento</label>
+                <Input name="birthdate" type="date" required/>
+              </ContentRow>
+            </Row>
+
+            <Buttons>
+              <Button type="submit">
+                Editar Dados
+              </Button>
+
+              <Link to='/home'>
+                Cancelar
+              </Link>
+            </Buttons>
+          </Form>
+        </Content>
+      </Wrapper>
+    </Container>
+  );
+}
+
+export default EditProfile;
---
+++ src/presentation/pages/EditProfile/styles.ts
@@ -0,0 +1,101 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  margin-left: 290px;
+`;
+
+export const Header = styled.div`
+  padding: 40px 34px 30px;
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  h3 {
+    margin-left: 24px;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+
+  button {
+    margin-top: 0px;
+    background: var(--cancel);
+
+    svg {
+      stroke: var(--white);
+
+      position: relative;
+      margin-right: 8px;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  flex: 1;
+  width: 100%;
+
+  padding: 4px 14px 0px 32px;
+
+  form label {
+    font-weight: 600;
+  }
+`;
+
+export const Row = styled.div`
+  display: flex;
+
+  @media(max-width: 986px) {
+    flex-direction: column;
+  }
+`;
+
+export const ContentRow = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  margin-right: 14px;
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content:center;
+
+  button, a {
+    margin: 14px;
+    background: var(--purpleLight);
+
+    padding: 14px 34px;
+
+    &:hover {
+      filter: brightness(85%);
+      transition: .35s ease-out;
+    }
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--white);
+
+    border-radius: 8px;
+    background: #FBB540;
+  }
+`;
+
